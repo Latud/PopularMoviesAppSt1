16271014580ca83fc27c40ab838911e3ac1690cb
Anna Tyulyakova <latud@yandex.ru>
2019-04-08 01:13:45 +0300

    added Detail Activity

--- app/src/main/java/com/example/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.popularmoviesapp;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,12 +28,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 import static com.example.popularmoviesapp.BuildConfig.API_KEY;
 
@@ -57,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
         mGridData = new ArrayList<>();
         mGridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, mGridData);
         mGridView.setAdapter(mGridAdapter);
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+
+                intent.putExtra("name", mGridData.get(position).getName());
+                intent.putExtra("overview", mGridData.get(position).getOverview());
+                intent.putExtra("posterPath", mGridData.get(position).getPosterPath());
+
+                startActivity(intent);
+
+            }
+        });
 
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("https")
@@ -90,6 +102,56 @@ public class MainActivity extends AppCompatActivity {
         mLoadingIndicator.setVisibility(View.INVISIBLE);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.popularSort) {
+
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https")
+                    .authority("api.themoviedb.org")
+                    .appendPath("3")
+                    .appendPath("movie")
+                    .appendPath("top_rated")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("language", "en-US");
+            urlString = builder.build().toString();
+
+            loadMoviesData();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+
+            return true;
+        }
+
+        if (id == R.id.ratingSort) {
+
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("https")
+                    .authority("api.themoviedb.org")
+                    .appendPath("3")
+                    .appendPath("movie")
+                    .appendPath("popular")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("language", "en-US")
+                    .appendQueryParameter("page", "1");
+            urlString = builder.build().toString();
+
+            loadMoviesData();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public class FetchMoviesTask extends AsyncTask<String, Void, Integer> {
 
         @Override
@@ -113,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // 200 represents HTTP OK
                 if (statusCode == 200) {
-                    String response = EntityUtils.toString(httpResponse.getEntity());;
+                    String response = EntityUtils.toString(httpResponse.getEntity());
                     parseResult(response);
                     result = 1; // Successful
                 } else {
@@ -175,55 +237,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.popularSort) {
-
-            Uri.Builder builder = new Uri.Builder();
-            builder.scheme("https")
-                    .authority("api.themoviedb.org")
-                    .appendPath("3")
-                    .appendPath("movie")
-                    .appendPath("top_rated")
-                    .appendQueryParameter("api_key", API_KEY)
-                    .appendQueryParameter("language", "en-US");
-            urlString = builder.build().toString();
-
-            loadMoviesData();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-
-            return true;
-        }
-
-        if (id == R.id.ratingSort) {
-
-            Uri.Builder builder = new Uri.Builder();
-            builder.scheme("https")
-                    .authority("api.themoviedb.org")
-                    .appendPath("3")
-                    .appendPath("movie")
-                    .appendPath("popular")
-                    .appendQueryParameter("api_key", API_KEY)
-                    .appendQueryParameter("language", "en-US")
-                    .appendQueryParameter("page", "1");
-            urlString = builder.build().toString();
-
-            loadMoviesData();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
 
 
